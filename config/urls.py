8f65b5f1af8ad7cb3120490a0607c2a8e3c6bcef
Neamar <neamar@neamar.fr>
2013-06-29 09:29:02 +0000

    Add documentation

--- config/urls.py
+++ config/urls.py
@@ -9,9 +9,6 @@ urlpatterns = patterns('',
 	# url(r'^$', 'kingdoms.views.home', name='home'),
 	# url(r'^kingdoms/', include('kingdoms.foo.urls')),
 
-	# Uncomment the admin/doc line below to enable admin documentation:
-	# url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-	# Uncomment the next line to enable the admin:
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 )
