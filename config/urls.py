802306757a479389d6adbb2d9f9ee5d9bdcab5e8
Amoki <hugoduroux@gmail.com>
2013-06-30 00:47:46 +0200

    improve views

--- config/urls.py
+++ config/urls.py
@@ -5,11 +5,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	# Examples:
-	# url(r'^$', 'kingdoms.views.home', name='home'),
-	# url(r'^kingdoms/', include('kingdoms.foo.urls')),
 
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^$', 'kingdom.views.index'),
+	url(r'^api/', 'kingdom.views.api.api'),
 )
---
+++ kingdom/views/api.py
@@ -0,0 +1,51 @@
+from django.utils import simplejson
+from django.http import HttpResponse
+from kingdom.models import Kingdom, Folk, ModalMessage, Message
+from kingdom.utils import *
+
+
+def api(request):
+	resp = {}
+
+	kingdom = Kingdom.objects.get(pk=1)
+	resp['kingdom'] = {
+		'population': kingdom.population,
+		'prestige': kingdom.prestige
+	}
+
+	folks = Folk.objects.filter(kingdom=kingdom)
+	resp['folks'] = []
+	for folk in folks:
+		resp['folks'].append({
+			'id': folk.id,
+			'name': folk.name,
+			'mother': folk.mother_id,
+			'father': folk.father_id,
+			'spouse': folk.spouse_id,
+			'birth': toTimestamp(folk.birth),
+			'death': toTimestamp(folk.death),
+			'fight': folk.fight,
+			'diplomacy': folk.diplomacy,
+			'plot': folk.plot,
+			'scholarship': folk.scholarship,
+			'loyalty': folk.loyalty
+		})
+
+	messages = Message.objects.filter(kingdom=kingdom)
+	resp['messages'] = []
+	for message in messages:
+		resp['folks'].append({
+			'content': message.content,
+			'level': message.level,
+			'read': message.read,
+			'creation': toTimestamp(message.creation)
+		})
+
+	modal_messages = ModalMessage.objects.filter(kingdom=kingdom)
+	resp['modal_messages'] = []
+	for modal_message in modal_messages:
+		resp['modal_messages'].append({
+			'creation': toTimestamp(modal_message.creation),
+		})
+
+	return HttpResponse(simplejson.dumps(resp))
