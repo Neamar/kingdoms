b8bf3f9777cd3900bb1f36f145aeb1f8c13b9b5e
Amoki <hugoduroux@gmail.com>
2013-06-29 23:27:28 +0200

    JSON response on /

--- config/urls.py
+++ config/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
 
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'kingdom.views.index'),
 )
--- kingdom/views.py
+++ kingdom/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.utils import simplejson
+from django.core import serializers
+from django.http import HttpResponse
+from kingdom.models import Kingdom, Folk, ModalMessage, Message
+
+
+def index(request):
+	resp = []
+	resp.append({"kingdom": serializers.serialize('json', Kingdom.objects.filter(pk=1), fields=('user', 'prestige', 'population'))})
+	resp.append({"folks": serializers.serialize('json', Folk.objects.filter(kingdom=Kingdom.objects.filter(pk=1)), fields=(
+		'sex', 'mother', 'father', 'spouse', 'fight', 'diplomacy', 'spot', 'scholarship', 'loyalty'))
+	})
+	resp.append({"messages": serializers.serialize('json', Message.objects.filter(kingdom=Kingdom.objects.filter(pk=1)), fields=(
+		'content', 'level', 'read', 'creation'))
+	})
+	resp.append({"modal_messages": serializers.serialize('json', ModalMessage.objects.filter(kingdom=Kingdom.objects.filter(pk=1)), fields=(
+		'creation'))
+	})
+	return HttpResponse(simplejson.dumps(resp))
