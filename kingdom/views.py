a1289bec0d32bac0163b9dff2bdef8afda41e528
Amoki <hugoduroux@gmail.com>
2013-06-29 23:30:42 +0200

    Improved json

--- kingdom/views.py
+++ kingdom/views.py
@@ -5,15 +5,20 @@ from kingdom.models import Kingdom, Folk, ModalMessage, Message
 
 
 def index(request):
-	resp = []
-	resp.append({"kingdom": serializers.serialize('json', Kingdom.objects.filter(pk=1), fields=('user', 'prestige', 'population'))})
-	resp.append({"folks": serializers.serialize('json', Folk.objects.filter(kingdom=Kingdom.objects.filter(pk=1)), fields=(
-		'sex', 'mother', 'father', 'spouse', 'fight', 'diplomacy', 'spot', 'scholarship', 'loyalty'))
-	})
-	resp.append({"messages": serializers.serialize('json', Message.objects.filter(kingdom=Kingdom.objects.filter(pk=1)), fields=(
-		'content', 'level', 'read', 'creation'))
-	})
-	resp.append({"modal_messages": serializers.serialize('json', ModalMessage.objects.filter(kingdom=Kingdom.objects.filter(pk=1)), fields=(
-		'creation'))
-	})
+	resp = {}
+
+	kingdom = Kingdom.objects.get(pk=1)
+	resp['kingdom'] = {
+		'population': kingdom.population,
+		'prestige': kingdom.prestige
+	}
+
+	folks = Folk.objects.filter(kingdom=kingdom)
+	resp['folks'] = []
+	for folk in folks:
+		resp['folks'].append({
+			'id': folk.id,
+			'name': folk.name
+		})
+
 	return HttpResponse(simplejson.dumps(resp))
