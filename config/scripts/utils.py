e51f2d0309dcbcbd8f2692aef57a4b1b6088d17f
Amoki <hugoduroux@gmail.com>
2013-06-30 22:50:08 +0200

    improve sum_folks

--- config/scripts/utils.py
+++ config/scripts/utils.py
@@ -1,28 +1,4 @@
 
 
 def sum_folks(folks, attribute):
-	if attribute == "fight":
-		fight_total = 0
-		for folk in folks:
-			fight_total += folk.fight
-		return fight_total
-	elif attribute == "diplomacy":
-		diplomacy_total = 0
-		for folk in folks:
-			diplomacy_total += folk.diplomacy
-		return diplomacy_total
-	elif attribute == "plot":
-		plot_total = 0
-		for folk in folks:
-			plot_total += folk.plot
-		return plot_total
-	elif attribute == "scholarship":
-		scholarship_total = 0
-		for folk in folks:
-			scholarship_total += 0
-		return scholarship_total
-	elif attribute == "loyalty":
-		loyalty_total = 0
-		for folk in folks:
-			loyalty_total += folk.loyalty
-		return loyalty_total
+	return sum([getattr(folk, attribute) for folk in folks])
