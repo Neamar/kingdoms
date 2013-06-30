48c3ca32678109de8f6aad2b1d6149c8bf68a979
Amoki <hugoduroux@gmail.com>
2013-06-30 21:55:27 +0200

    add tests for event

--- event/tests.py
+++ event/tests.py
@@ -36,6 +36,20 @@ class UnitTest(TestCase):
 		)
 		self.a.save()
 		
+		self.pe = PendingEvent(
+			event=self.e1,
+			kingdom=self.k,
+			text="some text"
+		)
+		self.pe.save()
+
+		self.pea = PendingEventAction(
+			pending_event=self.pe,
+			event_action=self.a,
+			text="some text"
+		)
+		self.pea.save()
+
 	def test_references_coherence(self):
 		"""
 		Check the pending_event and the event_action always refers to the same event.
@@ -54,3 +68,53 @@ class UnitTest(TestCase):
 		)
 
 		self.assertRaises(ValidationError, self.pea.save)
+
+	def test_condition_event(self):
+		"""
+		Check condition is triggered.
+		"""
+		self.pe.delete()
+		self.e1.condition = """
+status="notAllowed"
+"""
+		self.pe = PendingEvent(
+			event=self.e1,
+			kingdom=self.k,
+			text="some text"
+		)
+		self.assertRaises(ValidationError, self.pe.save)
+
+	def test_on_fire_event(self):
+		"""
+		Check the on-fire code
+		"""
+		self.e1.on_fire = """
+kingdom.population = 10
+kingdom.save()
+"""
+		self.e1.save()
+		self.pe.save()
+		self.assertEqual(self.k.population, 10)
+
+	def test_on_fire_action(self):
+		"""
+		Check the on_fire event_action
+		"""
+		self.a.on_fire = """
+kingdom.money=50
+kingdom.save()
+"""
+		self.a.save()
+
+		self.pea.fire()
+
+		self.assertEqual(self.k.money, 50)
+
+	def test_resolution_delete_pending_event(self):
+		# Pending event must be deleted
+		self.a.on_fire = """
+kingdom.money=50
+kingdom.save()
+"""
+		self.pea.fire()
+		self.assertRaises(PendingEvent.DoesNotExist, (lambda: PendingEvent.objects.get(pk=self.pe.pk)))
