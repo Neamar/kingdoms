da5302c3a3ba89035bdca117c2236bf7f485a0c9
Neamar <neamar@neamar.fr>
2013-06-30 07:48:27 +0000

    Improve admin for kingdom

--- kingdom/admin.py
+++ kingdom/admin.py
@@ -10,12 +10,29 @@ class KingdomAdmin(admin.ModelAdmin):
 admin.site.register(Kingdom, KingdomAdmin)
 
 
+class EventActionAdminInline(admin.StackedInline):
+	model = Quality
+	extra = 2
+
+
 class FolkAdmin(admin.ModelAdmin):
 	list_display = ('name', 'sex')
 admin.site.register(Folk, FolkAdmin)
 
 
 admin.site.register(Quality, DescribedModelAdmin)
-admin.site.register(Message)
+
+
+class MessageAdmin(admin.ModelAdmin):
+	list_display = ('kingdom', 'content', 'level', 'read')
+admin.site.register(Message, MessageAdmin)
+
+
+class ModalMessageAdmin(admin.ModelAdmin):
+	list_display = ('kingdom', 'name', 'description')
 admin.site.register(ModalMessage)
+
+
+class ClaimAdmin(admin.ModelAdmin):
+	list_display = ('offender', 'offended', 'creation')
 admin.site.register(Claim)
