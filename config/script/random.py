6ce36d7258cc583ba7b37f199a41cb48a8d99ac6
Amoki <hugoduroux@gmail.com>
2013-06-30 16:56:56 +0200

    improve random between

--- config/script/random.py
+++ config/script/random.py
@@ -1,12 +1,9 @@
 import random
 
 
-def random_between(a, b):
-	if random.randint(1, 2) == 1:
-		return a
-	else:
-		return b
+def random_between(tab):
+	return tab[random.randint(0, len(tab)-1)]
 
 
-def get_random_value(min, max):
+def random_value(min, max):
 	return random.randint(min, max)
