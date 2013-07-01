1ad6da088d0f0ad9a575d31d24460437f06a0966
Neamar <neamar@neamar.fr>
2013-07-01 17:17:13 +0000

    Refactor login

--- kingdom/views/index.py
+++ kingdom/views/index.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render_to_response
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as login_user
 
 
-def login_user(request):
+def login(request):
 	state = "Please log in below..."
 	username = password = ''
 	if request.POST:
@@ -12,11 +12,11 @@ def login_user(request):
 		user = authenticate(username=username, password=password)
 		if user is not None:
 			if user.is_active:
-				login(request, user)
+				login_user(request, user)
 				state = "You're successfully logged in!"
 			else:
 				state = "Your account is not active, please contact the site admin."
 		else:
 				state = "Your username and/or password were incorrect."
 
-	return render_to_response('index.html', {'state': state, 'username': username})
+	return render_to_response('login.html', {'state': state, 'username': username})
