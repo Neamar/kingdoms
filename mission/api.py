6406f8601db10ca76e45d3f070f06d178297d2e0
Neamar <neamar@neamar.fr>
2013-07-01 17:48:20 +0000

    Serialize missions

--- mission/api.py
+++ mission/api.py
@@ -1,5 +1,5 @@
 from mission.models import PendingMission, AvailableMission
-from mission.serializers import serialize_pending_mission
+from mission.serializers import serialize_pending_mission, serialize_available_mission
 
 
 def mission_api(request):
@@ -7,7 +7,21 @@ def mission_api(request):
 	JSON contribution from this app.
 	"""
 
-	resp = {"hello":True}
+	resp = {}
+
+	# Pending missions
+	pending_missions = PendingMission.objects.filter(kingdom=request.user.kingdom).select_related("mission")
+
+	resp['pending_missions'] = []
+	for pending_mission in pending_missions:
+		resp['pending_missions'].append(serialize_pending_mission(pending_mission))
+
+	# Available missions
+	available_missions = AvailableMission.objects.filter(kingdom=request.user.kingdom).select_related("mission")
+
+	resp['available_missions'] = []
+	for available_mission in available_missions:
+		resp['available_missions'].append(serialize_available_mission(available_mission))
 
 	return resp
 
--- mission/serializers.py
+++ mission/serializers.py
@@ -1,50 +1,30 @@
 def serialize_pending_mission(pending_mission):
 	"""
-	Serialize a kingdom object to JSON.
+	Serialize a pending mission object to JSON.
 	"""
 
 	r = {
 		'created': pending_mission.created,
 		'started': pending_mission.started,
+		'name': pending_mission.mission.name,
+		'duration': pending_mission.mission.duration,
+		'timeout': pending_mission.mission.timeout,
+		'cancellable': pending_mission.mission.cancellable
 	}
 
 	return r
 
 
-def serialize_folk(folk):
+def serialize_available_mission(available_mission):
 	"""
-	Serialize a folk object to JSON.
+	Serialize an available mission object to JSON.
 	"""
-	
+
 	r = {
-			'id': folk.id,
-			'name': folk.name,
-			'mother': folk.mother_id,
-			'father': folk.father_id,
-			'spouse': folk.spouse_id,
-			'mentor': folk.mentor_id,
-			'birth': folk.birth,
-			'death': folk.death,
-			'fight': folk.fight,
-			'diplomacy': folk.diplomacy,
-			'plot': folk.plot,
-			'scholarship': folk.scholarship,
-			'loyalty': folk.loyalty,
-	}
-
-	return r
-
-
-def serialize_message(message):
-	"""
-	Serialize a message object to JSON.
-	"""
-	
-	r = {
-			'content': message.content,
-			'level': message.level,
-			'read': message.read,
-			'creation': message.creation,
+		'name': available_mission.mission.name,
+		'duration': available_mission.mission.duration,
+		'timeout': available_mission.mission.timeout,
+		'cancellable': available_mission.mission.cancellable
 	}
 
 	return r
