5891e8835eaf9791c7f1830b923c9b00d1f488c5
Neamar <neamar@neamar.fr>
2013-07-01 15:32:54 +0000

    Add foundation code

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
-	url(r'^api/', 'kingdom.views.api.api'),
+	url(r'^api/$', 'kingdom.views.api.api'),
+
+	url(r'^app/$', TemplateView.as_view(template_name="app/index.html")),
 )
