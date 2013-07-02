361e242971dae6c56878d9ad575fc93ef8151915
Neamar <neamar@neamar.fr>
2013-07-02 20:28:48 +0000

    All clear!

--- event/views.py
+++ event/views.py
@@ -10,11 +10,11 @@ def pending_event_action_fire(request, pk):
 	"""
 	Fire the specified pending event
 	"""
-	if not request.POST:
-		raise Http404("Only call this URL by POST.")
+	#if not request.POST:
+	#	raise Http404("Only call this URL by POST.")
 
 	# Retrieve the object
-	pending_event_action = get_object_or_404(pk=pk, kingdom=request.user.kingdom_id)
+	pending_event_action = get_object_or_404(PendingEventAction, pk=pk, pending_event__kingdom=request.user.kingdom)
 
 	# Execute code
 	status = pending_event_action.fire()
