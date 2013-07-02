e1c0711e2cb024882f7381b22ce9a1e2bc68f263
Neamar <neamar@neamar.fr>
2013-07-02 20:25:56 +0000

    Fire code

--- event/views.py
+++ event/views.py
@@ -1,13 +1,22 @@
-from kingdom.decorators import json_view
 from django.http import Http404
+from django.shortcuts import get_object_or_404
+
+from kingdom.decorators import json_view
+from event.models import PendingEventAction
 
 
 @json_view
-def pending_event_action_fire(request):
+def pending_event_action_fire(request, pk):
 	"""
 	Fire the specified pending event
 	"""
 	if not request.POST:
 		raise Http404("Only call this URL by POST.")
 
-	return {'status': 'ok'}
+	# Retrieve the object
+	pending_event_action = get_object_or_404(pk=pk, kingdom=request.user.kingdom_id)
+
+	# Execute code
+	status = pending_event_action.fire()
+
+	return {'status': status}
