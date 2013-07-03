e701fddd586037afcd33ec601b5137cc657e7066
Neamar <neamar@neamar.fr>
2013-07-04 00:44:40 +0200

    Event dependencies

--- kingdom/management/commands/dependencies.py
+++ kingdom/management/commands/dependencies.py
@@ -17,11 +17,41 @@ class Command(BaseCommand):
 	dependencies = defaultdict(list)
 
 	def handle(self, *args, **options):
-		print "Loading events..."
-		events = Event.objects.all()
+		pending_event_slug = re.compile("Event.+slug=\"([a-z_]+)\"")
+		next_event_slug = re.compile("next_event\(\"([a-z_]+)\"\)")
+		event_regexps = [pending_event_slug, next_event_slug]
 
+		print "Loading events..."
+		events = Event.objects.all().prefetch_related('eventaction_set')
+
+		print "Parsing events..."
+		
 		for event in events:
-			pass
+			deps = []
+
+			deps += self._read_script(event.on_fire, event_regexps)
+			for event_action in event.eventaction_set.all():
+				deps += self._read_script(event_action.on_fire, event_regexps)
+
+			for dep in set(deps):
+				self.dependencies[event.slug].append(dep)
+
+		# Output results
+		print "Generating results..."
+		out = self._output_dot()
+		print "------------"
+		self.stdout.write(out)
+
+	def _read_script(self, code, regexps):
+		"""
+		Read code, looking for dependencies in regexp.
+		"""
+		deps = []
+
+		for regexp in regexps:
+			deps += regexp.findall(code)
+
+		return deps
 
 	def _add_dependencies(self, dfrom, dto):
 		"""
