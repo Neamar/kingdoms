1e65181b6714da746b92576d13b4c3a23918423c
Neamar <neamar@neamar.fr>
2013-07-03 19:04:25 +0200

    Display mission grids

--- mission/serializers.py
+++ mission/serializers.py
@@ -15,7 +15,7 @@ def serialize_pending_mission(pending_mission):
 		'duration': pending_mission.mission.duration,
 		'timeout': pending_mission.mission.timeout,
 		'cancellable': pending_mission.mission.cancellable,
-		'grids': [serialize_mission_grid(o) for o in pending_mission.mission.missiongrid_set.all()],
+		'grids': [serialize_mission_grid(o, pending_mission) for o in pending_mission.mission.missiongrid_set.all()],
 		'has_target': pending_mission.mission.has_target,
 	}
 
@@ -37,15 +37,18 @@ def serialize_target(kingdom):
 	return r
 
 
-def serialize_mission_grid(mission_grid):
+def serialize_mission_grid(mission_grid, pending_mission):
 	"""
-	Serialize an available mission object to JSON.
+	Serialize a mission grid object to JSON.
+	Needs the current pending mission to display the affectation.
 	"""
+	affectations = [o.folk_id for o in pending_mission.folk_set.all()]
+	affectations += [None]*(mission_grid.length-len(affectations))
 
 	r = {
 		'id': mission_grid.id,
 		'name': mission_grid.name,
-		'length': mission_grid.length,
+		'raw_affectations': affectations
 	}
 
 	return r
