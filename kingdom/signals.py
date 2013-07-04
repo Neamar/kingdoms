8cae790ae93268c27c1d8dc7e2055b4c553fe5e5
Neamar <neamar@neamar.fr>
2013-07-05 01:45:50 +0200

    Constrains kingdom values to be positive

--- kingdom/signals.py
+++ kingdom/signals.py
@@ -2,8 +2,21 @@ from django.db.models.signals import pre_save, m2m_changed
 from django.dispatch import receiver
 from django.core.exceptions import ValidationError
 
-from kingdom.models import Folk, Quality
+from kingdom.models import Kingdom, Folk, Quality
 from config.lib.execute import execute
+
+
+@receiver(pre_save, sender=Kingdom)
+def kingdom_attributes_constraints(sender, instance, **kwargs):
+	"""
+	Constrains attributes between 0 and their max values.
+
+	Validators are not enough, since ScriptField can access raw values.
+	"""
+
+	instance.money = max(0, instance.money)
+	instance.population = max(0, instance.population)
+	instance.prestige = max(0, instance.prestige)
 
 
 @receiver(pre_save, sender=Folk)
