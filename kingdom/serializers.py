c51e3d8768070531f53c9a17351f90be4246212b
Neamar <neamar@neamar.fr>
2013-07-06 00:49:42 +0200

    Serializers returns avatar

--- kingdom/serializers.py
+++ kingdom/serializers.py
@@ -13,17 +13,12 @@ def serialize_kingdom(kingdom):
 	return r
 
 
-colors = ("000", "F00", "0F0", "00F", "FF0", "0FF", "FFF", "AAA")
 def serialize_folk(folk):
 	"""
 	Serialize a folk object to JSON.
 	"""
 
 	r = {
-		'id': folk.pk,
-		'avatar': "http://placehold.it/100x120/" + colors[folk.pk % len(colors)],
-		'first_name': folk.first_name,
-		'last_name': folk.last_name,
 		'mother': folk.mother_id,
 		'father': folk.father_id,
 		'spouse': folk.spouse_id,
@@ -38,6 +33,8 @@ def serialize_folk(folk):
 		'raw_qualities': [q.pk for q in folk.quality_set.all()]
 	}
 
+	r.update(serialize_folk_min(folk))
+
 	return r
 
 
@@ -45,12 +42,15 @@ def serialize_folk_min(folk):
 	"""
 	Serialize a folk object to JSON, with minimal information.
 	"""
-	
+	colors = ("000", "F00", "0F0", "00F", "FF0", "0FF", "FFF", "AAA", "AF0", "A0F", "FA0", "F0A", "0AF", "0FA")
+
+	image = folk.avatar.image.url if folk.avatar else "http://placehold.it/100x120/" + colors[folk.pk % len(colors)]
+
 	r = {
 		'id': folk.pk,
+		'avatar': image,
 		'first_name': folk.first_name,
 		'last_name': folk.last_name,
-		'avatar': "http://placehold.it/100x120/" + colors[folk.pk % len(colors)],
 	}
 
 	return r
