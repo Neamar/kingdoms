0652ef32af812e50f3a98ac02c7e2117d9d11f5f
Neamar <neamar@neamar.fr>
2013-07-06 15:16:26 +0200

    Add unique slug for title

--- title/models.py
+++ title/models.py
@@ -12,6 +12,8 @@ class Title(DescribedModel):
 	Dictionary of all titles in the game
 	"""
 
+	slug = models.SlugField(unique=True)
+
 	on_unlock = ScriptField(blank=True, null=True, help_text="Called after kingdom unlock. `param` is the current `AvailableTitle`, `kingdom` is the current kingdom.", default=None)
 
 	condition = ScriptField(blank=True, null=True, help_text="Called before folk nomination. `param` is the current `AvailableTitle`, `folk` is the folk to be affected. Have the script set `status`to something other than 'ok' to abort the affectation.", default=None)
