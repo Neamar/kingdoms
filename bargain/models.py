5b58a615d40fe1f374bd9b8dc1c3249831453336
Neamar <neamar@neamar.fr>
2013-07-06 11:17:43 +0200

    Model definition

--- bargain/models.py
+++ bargain/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+from kingdom.models import Kingdom, Folk
+from mission.models import PendingMission
+
+
+class PendingBargain(models.Model):
+	"""
+	A bargain between two players, to share missions.
+	"""
+
+	started = models.DateTimeField(auto_now_add=True)
+
+
+class PendingBargainKingdom(models.Model):
+	"""
+	The kingdoms part of the negotiation
+	"""
+
+	PENDING = 0
+	OK = 1
+	OK_NO_MATTER_WHAT = 2
+
+	STATE_CHOICES = (
+		(PENDING, 'Négociation en cours'),
+		(OK, 'Négociation validée')
+		(OK_NO_MATTER_WHAT, 'Négociation validée sans condition')
+	)
+
+	pending_bargain = models.ForeignKey(PendingBargain)
+
+	kingdom = models.ForeignKey(Kingdom)
+	state = models.PositiveIntegerField(choices=STATE_CHOICES, default=PENDING)
+
+
+class PendingBargainSharedMission(models.Model):
+	"""
+	A shared mission between the two parts of the bargain.
+	"""
+
+	pending_bargain = models.ForeignKey(PendingBargain)
+
+	pending_mission = models.ForeignKey(PendingMission)
+
+
+class PendingBargainSharedMissionAffectation(models.Model):
+	"""
+	Proposition of affectation to the pending mission.
+	While the bargaining is running, those affectations are purely virtual.
+	"""
+
+	pending_bargain_shared_mission = models.ForeignKey(PendingBargainSharedMission)
+
+	folk = models.ForeignKey(Folk)
