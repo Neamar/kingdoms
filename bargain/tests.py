8e10f79023416e33499fc50d7235f6148b56386a
Neamar <neamar@neamar.fr>
2013-07-06 11:26:45 +0200

    Bargain test skeleton

--- bargain/tests.py
+++ bargain/tests.py
@@ -1,1 +1,30 @@
 from django.test import TestCase
+
+from kingdom.models import Kingdom, Folk
+from mission.models import Mission, MissionGrid, PendingMission
+from bargain.models import PendingBargain, PendingBargainKingdom, PendingBargainSharedMission, PendingBargainSharedMissionAffectation
+
+
+class UnitTest(TestCase):
+	def setUp(self):
+		self.k = Kingdom()
+		self.k.save()
+
+		self.f = Folk(kingdom=self.k)
+		self.f.save()
+
+		self.m = Mission(
+			name="Stub mission",
+			slug="stub",
+			on_resolution="",
+			title=self.t,
+		)
+		self.m.save()
+
+		self.mg = MissionGrid(
+			mission=self.m,
+		)
+		self.mg.save()
+
+		self.pb = PendingBargain()
+		self.pb.save()
