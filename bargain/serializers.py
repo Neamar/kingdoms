83bf738cc2515a9b6b32d11d716b98be141dc1d8
Neamar <neamar@neamar.fr>
2013-07-06 17:48:47 +0200

    Add state

--- bargain/serializers.py
+++ bargain/serializers.py
@@ -8,11 +8,22 @@ def serialize_pending_bargain(pending_bargain, kingdom):
 	Serialize a pending event object to JSON.
 	"""
 
-	partner = pending_bargain.pendingbargainkingdom_set.exclude(kingdom=kingdom).get()
+	parties = pending_bargain.pendingbargainkingdom_set.all()
+
+	partner = None
+	me = None
+	for p in parties:
+		if p.kingdom_id == kingdom.id:
+			me = p
+		else:
+			partner = p
+
 	r = {
 		'id': pending_bargain.pk,
 		'started': pending_bargain.started,
+		'state': me.state,
 		'partner': partner.kingdom.user.username,
+		'partner_state': partner.state,
 		'shared_missions': [serialize_pending_mission(o) for o in pending_bargain.pendingbargainsharedmission_set.all()]
 	}
 
