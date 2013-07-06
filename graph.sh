1e884f2795e2493ee12e2ab4b284f5160828f5a8
Neamar <neamar@neamar.fr>
2013-07-06 11:19:30 +0200

    Update graph model generation

--- graph.sh
+++ graph.sh
@@ -1,4 +1,4 @@
-python manage.py graph_models kingdom internal title mission event -o models.png
+python manage.py graph_models kingdom internal title mission event bargain -o models.png
 
 # App
 python manage.py graph_models kingdom -g -o kingdom/models.png
@@ -6,3 +6,4 @@ python manage.py graph_models internal -g -o internal/models.png
 python manage.py graph_models title -g -o title/models.png
 python manage.py graph_models mission -g -o mission/models.png
 python manage.py graph_models event -g -o event/models.png
+python manage.py graph_models bargain -g -o bargain/models.png
