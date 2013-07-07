a4aabdf5f2f4a5dc4b441e63f9185ebd0720f30c
Neamar <neamar@neamar.fr>
2013-07-07 19:32:55 +0200

    Use pygraphviz for dependencies

--- kingdom/management/commands/dependencies.py
+++ kingdom/management/commands/dependencies.py
@@ -2,6 +2,7 @@
 Generate a dot file for dependencies between Event and Mission.
 """
 import re
+import pygraphviz as pgv
 
 from collections import defaultdict
 from django.core.management.base import BaseCommand
@@ -14,8 +15,7 @@ class Command(BaseCommand):
 	args = ''
 	help = 'Generate dependencies graph for Event and Missions'
 
-	objects = {}
-	dependencies = []
+	graph = None
 
 	pending_event_slug = re.compile("PendingEvent.+slug=\"(\w+)\"")
 	next_event_object = re.compile("next_event.+slug=\"(\w+)")
@@ -27,36 +27,37 @@ class Command(BaseCommand):
 	mission_regexps = [pending_mission_slug, unlock_mission]
 
 	def handle(self, *args, **options):
-		self.dependencies = defaultdict(list)
-		self.objects = {}
+		self.graph = pgv.AGraph(strict=False, directed=True)
 
 		events = Event.objects.all().prefetch_related('eventaction_set')
 
 		for event in events:
-			self.objects["event_" + event.slug] = (event.slug, event.name)
+			self.graph.add_node("event_" + event.slug, color="lightblue2", style="filled")
+			
+		missions = Mission.objects.all()
+		for mission in missions:
+			self.graph.add_node("mission_" + mission.slug, color="lightyellow2", style="filled")
 
+		for event in events:
 			deps = []
 			deps += self._read_script(event.on_fire)
 			for event_action in event.eventaction_set.all():
 				deps += self._read_script(event_action.on_fire)
 
 			for dep in set(deps):
-				self.dependencies['event_' + event.slug].append(dep)
-
-		missions = Mission.objects.all()
+				self.graph.add_edge('event_' + event.slug, dep)
+		
 		for mission in missions:
-			self.objects["mission_" + mission.slug] = (mission.slug, mission.name)
 			deps = []
-
 			deps += self._read_script(mission.on_init)
 			deps += self._read_script(mission.on_start)
 			deps += self._read_script(mission.on_resolution)
 
 			for dep in set(deps):
-				self.dependencies['mission_' + mission.slug].append(dep)
+				self.graph.add_edge('mission_' + mission.slug, dep)
 
 		# Output results
-		out = self._output_dot()
+		out = self.graph.string()
 		self.stdout.write(out)
 
 	def _read_script(self, code):
@@ -75,40 +76,3 @@ class Command(BaseCommand):
 			deps += ["mission_" + m for m in regexp.findall(code)]
 
 		return deps
-
-	def _add_dependencies(self, dfrom, dto):
-		"""
-		Add a dependency of dfrom towards dto.
-		"""
-		self.dependencies[dfrom].append(dto)
-
-	def _output_dot(self):
-		"""
-		Return dot files for current dependencies.
-		"""
-
-		events = ';'.join([o for o in self.objects.keys() if o.startswith('event_')])
-		missions = ';'.join([o for o in self.objects.keys() if o.startswith('mission_')])
-
-		ret = """digraph name {\n
-	fontname = "Helvetica"
-	fontsize = 8
-	node [color=lightblue2, style=filled]; %s
-	node [color=lightyellow2, style=filled]; %s
-	node [color=red, style=filled];
-""" % (events, missions)
-
-		for k, o in self.objects.items():
-			ret += """%s [label=<
-	<FONT FACE="Helvetica Bold">%s</FONT>
-	<br />
-	<FONT FACE="Helvetica Italic">%s</FONT>
-		>]\n\n""" % (k, o[1], o[0])
-
-		for k, vs in self.dependencies.items():
-			for v in vs:
-				ret += "	%s->%s\n" % (k, v)
-
-		ret += "}"
-
-		return ret
