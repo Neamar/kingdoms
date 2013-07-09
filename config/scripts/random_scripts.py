8db06951d0bf3f5f61434aef067ec8832a368fba
Neamar <neamar@neamar.fr>
2013-07-09 17:27:05 +0200

    Add some more random functions : random_die and fuzzy

--- config/scripts/random_scripts.py
+++ config/scripts/random_scripts.py
@@ -2,8 +2,33 @@ from random import randint
 
 
 def random_in(tab):
+	"""
+	Return a random value in the specified array.
+	"""
+
 	return tab[randint(0, len(tab)-1)]
 
 
 def random_value(min, max):
+	"""
+	Return a value between min and max.
+	"""
+
 	return randint(min, max)
+
+
+def random_die(max):
+	"""
+	Returns the results of a die.
+	"""
+
+	return randint(0, max)
+
+
+def fuzzy(max):
+	"""
+	Returns the results of a die minus another die.
+	(Feng-shui style)
+	"""
+
+	return random_die(max) - random_die(max)
