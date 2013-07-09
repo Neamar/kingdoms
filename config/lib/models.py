b5f3b7016b1b64de2a63bc8cbd6841e160654927
Neamar <neamar@neamar.fr>
2013-07-09 18:01:53 +0200

    Add a layer of abstraction around execute.

--- config/lib/models.py
+++ config/lib/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class NamedModel(models.Model):
+	"""
+	Model with a unique name.
+	"""
+
 	class Meta:
 		abstract = True
 
@@ -12,7 +16,40 @@ class NamedModel(models.Model):
 
 
 class DescribedModel(NamedModel):
+	"""
+	Model with unique name and description.
+	"""
+
 	class Meta:
 		abstract = True
 
 	description = models.TextField()
+
+
+class ScriptedModel(models.Model):
+	"""
+	Model with the ability to run some code.
+	"""
+	class Meta:
+		abstract = True
+
+	def execute(self, model, attr, kingdom, raw_context=None):
+		"""
+		Execute the code stored in :attr on :model object, with :self as param. Context is built by default with :kingdom key, additional values can be passed with raw_context.
+		"""
+
+		from config.lib.execute import execute
+
+		# Build context object
+		context = {
+			'kingdom': kingdom,
+			'folks': kingdom.folk_set.all(),
+		}
+
+		if raw_context is not None:
+			context.update(raw_context)
+
+		# Execute code
+		status, param = execute(getattr(model, attr), self, context)
+
+		return status, param
