2d3d6c1b027b3e180e5e9c4b806c11032530f550
Neamar <neamar@neamar.fr>
2013-07-14 19:08:38 +0200

    Cron command

--- kingdom/management/commands/cron.py
+++ kingdom/management/commands/cron.py
@@ -1,11 +1,12 @@
 """
 Calls cron commands
 """
-import datetime
+import time
 import django.dispatch
+from django.core.management.base import BaseCommand
 
-cron_minute = django.dispatch.Signal(providing_args=[])
-cron_ten_minutes = django.dispatch.Signal(providing_args=[])
+cron_minute = django.dispatch.Signal(providing_args=['counter'])
+cron_ten_minutes = django.dispatch.Signal(providing_args=['counter'])
 
 
 class Command(BaseCommand):
@@ -13,4 +14,8 @@ class Command(BaseCommand):
 	help = 'Calls crons from each module. This script must be called by a crontab.'
 
 	def handle(self, *args, **options):
-		cron_minute.send()
+		counter = int(time.time() / 60)
+		cron_minute.send(self, counter=counter)
+
+		if counter % 10 == 0:
+			cron_ten_minutes.send(self, counter=counter / 10)
