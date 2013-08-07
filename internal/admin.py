a8f542a59c0c4ff841baf637669f5689586a9cfc
Neamar <neamar@neamar.fr>
2013-08-07 14:47:27 +0200

    Update admins for internal and reporting

--- internal/admin.py
+++ internal/admin.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from internal.models import Trigger, Constant, Recurring, FirstName, LastName, Function, Avatar, ScriptLog
+from internal.models import Trigger, Constant, Recurring, FirstName, LastName, Function, Avatar
 from kingdom.models import Kingdom
 
 
@@ -66,36 +66,3 @@ class AvatarAdmin(admin.ModelAdmin):
 	thumb_old.allow_tags = True
 admin.site.register(Avatar, AvatarAdmin)
 
-
-class ScriptLogAdmin(admin.ModelAdmin):
-	list_display = ('slug', 'object_type', 'object_pk', 'object_attr', 'kingdom', 'time', 'queries', 'direct_queries')
-	list_filter = ('object_type', 'object_attr')
-
-	def has_add_permission(self, request, obj=None):
-		return False
-
-	def slug(self, obj):
-		from event.models import Event, EventAction
-		from mission.models import Mission, MissionGrid
-		from title.models import Title
-		from kingdom.models import Quality
-		from internal.models import Trigger
-
-		classes = {
-			'Event': lambda pk: Event.objects.get(pk=pk).slug,
-			'EventAction': lambda pk: EventAction.objects.get(pk=pk).event.slug,
-			'Mission': lambda pk: Mission.objects.get(pk=pk).slug,
-			'MissionGrid': lambda pk: MissionGrid.objects.get(pk=pk).mission.slug + MissionGrid.objects.get(pk=pk).name,
-			'Function': lambda pk: Function.objects.get(pk=pk).slug,
-			'Title': lambda pk: Title.objects.get(pk=pk).slug,
-			'Quality': lambda pk: Quality.objects.get(pk=pk).slug,
-			'Trigger': lambda pk: Trigger.objects.get(pk=pk).slug
-		}
-
-		if obj.object_type in classes.keys():
-			indentation = '&nbsp;&nbsp;&nbsp;' * (obj.stack_level - 1)
-			slug = classes[obj.object_type](obj.object_pk)
-			return indentation + slug
-		return '--'
-	slug.allow_tags = True
-admin.site.register(ScriptLog, ScriptLogAdmin)
