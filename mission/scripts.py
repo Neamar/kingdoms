10c751ab0aa79e48e9358e3ec5c6f1fa13914cb7
Neamar <neamar@neamar.fr>
2013-09-01 00:21:16 +0200

    Create pending mission from kingdom

--- mission/scripts.py
+++ mission/scripts.py
@@ -6,7 +6,7 @@ from django.db import IntegrityError
 from django.db import transaction
 
 from kingdom.models import Kingdom
-from mission.models import Mission, AvailableMission
+from mission.models import Mission, PendingMission, AvailableMission
 
 
 def kingdom_unlock_mission(self, slug):
@@ -28,3 +28,19 @@ def kingdom_unlock_mission(self, slug):
 		transaction.savepoint_rollback(sid)
 		pass
 Kingdom.unlock_mission = kingdom_unlock_mission
+
+
+def kingdom_create_pending_mission(self, slug):
+	"""
+	Create a pending mission on this kingdom.
+	"""
+	mission = Mission.objects.get(slug=slug)
+	pm = PendingMission(
+		kingdom=self,
+		mission=mission
+	)
+
+	pm.save()
+
+	return pm
+Kingdom.create_pending_mission = kingdom_create_pending_mission
--- mission/tests/script.py
+++ mission/tests/script.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 from kingdom.models import Kingdom, Folk
-from mission.models import Mission, AvailableMission
+from mission.models import Mission, AvailableMission, PendingMission
 from mission.scripts import *
 
 
@@ -32,13 +32,19 @@ class ScriptTest(TestCase):
 		Check the available mission is created.
 		"""
 
-		# Sanity check
-		self.assertRaises(AvailableMission.DoesNotExist, (lambda: AvailableMission.objects.get(kingdom=self.k, mission=self.m)))
-
 		self.k.unlock_mission("stub")
 		
 		# AssertNoRaises
 		AvailableMission.objects.get(kingdom=self.k, mission=self.m)
+
+	def test_kingdom_create_pending_mission(self):
+		"""
+		Check the pending mission is created.
+		"""
+
+		self.k.create_pending_mission("stub")
+		
+		self.assertEqual(self.k.pendingmission_set.count(), 1)
 
 	def test_kingdom_unlock_title_already_available(self):
 		"""
