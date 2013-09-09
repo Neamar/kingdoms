f5b8e44ad45f4f754f26988b3c4ca38369f180a0
Neamar <neamar@neamar.fr>
2013-09-09 22:48:47 +0200

    Move execute() tests in kingdom

--- kingdom/tests/__init__.py
+++ kingdom/tests/__init__.py
@@ -1,3 +1,4 @@
 from kingdom.tests.unit import *
 from kingdom.tests.scripts import *
 from kingdom.tests.api import *
+from kingdom.tests.code import *
--- title/tests/__init__.py
+++ title/tests/__init__.py
@@ -1,3 +1,2 @@
 from title.tests.unit import *
-from title.tests.code import *
 from title.tests.script import *
