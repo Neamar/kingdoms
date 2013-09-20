c2d0eb54663f0c66037166f24d5ef0c01c7cdb2f
Neamar <neamar@neamar.fr>
2013-09-20 10:36:51 +0200

    Add random_gauss value

--- config/scripts/random_scripts.py
+++ config/scripts/random_scripts.py
@@ -17,6 +17,17 @@ def random_value(min, max):
 	return randint(min, max)
 
 
+def random_gauss(min, max, mu, sigma): 
+	"""
+	Return a random number from a truncated Gaussian distribution.
+	"""
+	
+	while 1: 
+		x = randint.gauss(mu, sigma) 
+		if min <= x <= max: 
+			return x 
+
+
 def random_die(max):
 	"""
 	Returns the results of a die.
