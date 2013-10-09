773351f5f9d3131302a20537bf0202b00a9222ac
Neamar <neamar@neamar.fr>
2013-10-09 22:36:04 +0200

    Doc for recurring cron

--- internal/signals.py
+++ internal/signals.py
@@ -94,7 +94,7 @@ def cron_fire_recurring(sender, counter, **kwargs):
 	Fire recurring on a delay basis.
 	"""
 
-	recurrings = Recurring.objects.extra(where=[str(int(counter)) + ' %% delay = 0'])
+	recurrings = Recurring.objects.extra(where=['%s %% delay = 0' % int(counter)])
 
 	for recurring in recurrings:
 		for kingdom in recurring.kingdoms():
