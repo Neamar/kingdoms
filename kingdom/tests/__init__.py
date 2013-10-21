33c3370d81c6b126be9569e908deb4fb7117d22c
Neamar <neamar@neamar.fr>
2013-10-22 00:14:22 +0200

    Separation of tests

--- kingdom/tests/__init__.py
+++ kingdom/tests/__init__.py
@@ -1,4 +1,6 @@
 from kingdom.tests.unit import *
 from kingdom.tests.scripts import *
 from kingdom.tests.api import *
+from kingdom.tests.view import *
+from kingdom.tests.command import *
 from kingdom.tests.code import *
---
+++ kingdom/tests/view.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+from django.test.client import Client
+from django.contrib.auth.models import User
+
+from kingdom.models import Kingdom
+
+class ViewTest(TestCase):
+	"""
+	Views tests for kingdom projects.
+	This is not complete. It only checks the return code.
+	"""
+
+	def setUp(self):
+		self.u = User(username="test")
+		self.u.set_password("pwd")
+		self.u.save()
+
+		self.k = Kingdom(user=self.u)
+		self.k.save()
+
+		self.c = Client()
+		self.c.login(username=self.u.username, password="pwd")
+
+	def test_app_require_login(self):
+		"""
+		Check app returns with status 200
+		"""
+
+		r = Client().get(reverse('kingdom.views.index.app'))
+		self.assertEqual(302, r.status_code)
+
+	def test_login_is_up(self):
+		"""
+		Check app returns with status 200
+		"""
+
+		r = self.c.get(reverse('django.contrib.auth.views.login'))
+		self.assertEqual(200, r.status_code)
+
+	def test_app_is_up(self):
+		"""
+		Check app returns with status 200
+		"""
+
+		r = self.c.get(reverse('kingdom.views.index.app'))
+		self.assertEqual(200, r.status_code)
+
+	def test_dependencies_is_up(self):
+		"""
+		Check app returns with status 200
+		"""
+
+		r = self.c.get(reverse('kingdom.views.index.app'))
+		self.assertEqual(200, r.status_code)
