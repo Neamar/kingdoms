a9cba1ffc75280b6c78816a298318d2af8307f99
Neamar <neamar@neamar.fr>
2013-10-22 00:23:05 +0200

    Replace command

---
+++ kingdom/tests/command.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.core import management
+from StringIO import StringIO 
+
+from internal.models import Trigger
+
+class CommandTest(TestCase):
+	"""
+	Test commands
+	"""
+
+	def setUp(self):
+		self.t = Trigger(
+			slug="test",
+			condition="",
+			on_fire="# Some code"
+		)
+		self.t.save()
+
+	def test_replace(self):
+		"""
+		Check replacements occurs.
+		"""
+
+		content = StringIO()
+		management.call_command('replace', 'co.?e', 'REPLACED', dry=True, interactive=False, stdout=content)
+
+		content.seek(0)
+		out = content.read()
+		self.assertTrue('1 object' in out)
+		self.assertTrue('Some REPLACED' in out)
