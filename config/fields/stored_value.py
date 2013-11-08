a3910bf77a627dbbd9887edbf9b5bfcbe13c537e
Neamar <neamar@neamar.fr>
2013-11-09 00:45:09 +0100

    Use JSON for stored_values

--- config/fields/stored_value.py
+++ config/fields/stored_value.py
@@ -5,6 +5,63 @@ from django.db import models
 from django.db.models.query import QuerySet
 from django.core.exceptions import ValidationError
 
+fk_regexp = re.compile("`\w*`:\w*")
+
+
+class CustomEncoder(json.JSONEncoder):
+	"""
+	Add some objects as JSON-encodable
+	"""
+	def default(self, obj):
+		if isinstance(obj, models.Model):
+			return self.encode_model(obj)
+		# Let the base class default method raise the TypeError
+		return json.JSONEncoder.default(self, obj)
+
+	def encode_model(self, item):
+		if item.pk is None:
+			raise ValidationError("ForeignKey must be saved before being stored.")
+
+		return {
+			'__type__': 'model',
+			'class': item.__class__.__name__,
+			'pk': item.pk
+		}
+
+
+class CustomDecoder:
+	"""
+	Add some objects as JSON-decodable
+	Warning; does not work intuitively.
+	See http://taketwoprogramming.blogspot.fr/2009/06/subclassing-jsonencoder-and-jsondecoder.html
+	"""
+	def decode(self, d):
+		if '__type__' not in d:
+			return d
+
+		if d['__type__'] == 'model':
+			return self.decode_model(d)
+
+	def decode_model(self, d):
+		from kingdom.models import Kingdom, Folk, Message, Claim, Quality
+		from internal.models import Constant
+		from event.models import Event, PendingEvent
+		from mission.models import Mission, PendingMission, PendingMissionAffectation
+		from title.models import Title, AvailableTitle
+
+		class_name = d['class']
+		instance_id = d['pk']
+
+		# Instantiate the class from its name
+		value_class = locals()[class_name]
+	
+		try:
+			instance = value_class.objects.get(id=instance_id)
+			return instance
+		except:
+			return None
+customDecoder = CustomDecoder()
+
 
 class StoredValueField(models.CharField):
 	"""
@@ -26,71 +83,12 @@ class StoredValueField(models.CharField):
 		super(StoredValueField, self).__init__(*args, **kwargs)
 
 	def to_python(self, value):
-		from kingdom.models import Kingdom, Folk, Message, Claim, Quality
-		from internal.models import Constant
-		from event.models import Event, PendingEvent
-		from mission.models import Mission, PendingMission, PendingMissionAffectation
-		from title.models import Title, AvailableTitle
-
-		if isinstance(value, (models.Model, list, tuple, dict, QuerySet)):
-			return value
-
-		if isinstance(value, basestring) and self.array_regexp.match(value):
-			# Array
-			return [self.to_python(raw) for raw in json.loads(value)]
-		elif isinstance(value, basestring) and self.object_regexp.match(value):
-			# Dict
-			return {key: self.to_python(v) for key, v in json.loads(value).items()}
-		elif isinstance(value, basestring) and self.fk_regexp.match(value):
-			# Foreign Key
-			class_name, instance_id = value.split(':')
-			class_name = class_name[1:-1]
-			instance_id = int(instance_id, 10)
-
-			# Instantiate the class from its name
-			value_class = locals()[class_name]
-		
-			try:
-				instance = value_class.objects.get(id=instance_id)
-				return instance
-			except:
-				return None
-		elif value is None:
-			return None
-		elif value == "`True`":
-			return True
-		elif value == "`False`":
-			return False
-		else:
-			try:
-				v = float(value)
-				if int(v) == v:
-					return int(v)
-				else:
-					return v
-			except ValueError:
-				return value
-			except TypeError:
-				raise ValidationError("Unable to store this data : %s." % value)
+		if isinstance(value, basestring) and value.startswith('JSON:'):
+			return json.loads(value[5:], object_hook=customDecoder.decode)
+		return value
 
 	def get_prep_value(self, value):
-		if isinstance(value, (list, tuple, QuerySet)):
-			return json.dumps([self.get_prep_value(v) for v in value])
-		elif isinstance(value, dict):
-			return json.dumps({key: self.get_prep_value(v) for key, v in value.items()})
-		elif isinstance(value, bool):
-			return "`%s`" % str(value)
-		if isinstance(value, (int, basestring, float)):
-			return value
-		elif isinstance(value, models.Model):
-			if value.pk is None:
-				raise ValidationError("ForeignKey must be saved before being stored.")
-			name = value.__class__.__name__
-			return '`%s`:%s' % (name, value.pk)
-		elif value is None:
-			return None
-		else:
-			raise ValidationError("Value must be int, string, bool, list or DB object : %s" % value)
+		return 'JSON:' + json.dumps(value, cls=CustomEncoder)
 
 	def value_to_string(self, obj):
 		value = self._get_val_from_obj(obj)
