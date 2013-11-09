8a0dff8d404bb0a41efbbb8e06feb22a64256516
Neamar <neamar@neamar.fr>
2013-11-09 15:44:26 +0100

    Problem in tests

--- kingdom/tests/view.py
+++ kingdom/tests/view.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.test.client import Client
 from django.contrib.auth.models import User
 
-from kingdom.models import Kingdom
+from kingdom.models import Kingdom, Quality, QualityCategory
 
 class ViewTest(TestCase):
 	"""
@@ -52,5 +52,32 @@ class ViewTest(TestCase):
 		Check app returns with status 200
 		"""
 
-		r = self.c.get(reverse('kingdom.views.index.app'))
+		r = self.c.get(reverse('kingdom.views.index.dependencies', args=('svg',)))
+		self.assertEqual(200, r.status_code)
+
+	def test_admin_is_up(self):
+		"""
+		Check admin returns with status 200
+		"""
+
+		r = self.c.get('/admin/')
+		self.assertEqual(200, r.status_code)
+
+	def test_admin_subpage_is_up(self):
+		"""
+		Check admin returns with status 200
+		"""
+		qc = QualityCategory(
+			name="category"
+		)
+		qc.save()
+
+		q = Quality(
+			category = qc,
+			slug="quality",
+			name="quality"
+		)
+		q.save()
+
+		r = self.c.get('/admin/kingdom/quality/1/')
 		self.assertEqual(200, r.status_code)
