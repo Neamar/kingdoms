ae088b2eb673eb06be331e786ecb35098f5dfb0b
Neamar <neamar@neamar.fr>
2013-11-09 15:24:02 +0100

    Random tests

--- config/tests/scripts.py
+++ config/tests/scripts.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 
 from config.scripts.random_scripts import random_in, random_value, random_gauss, random_die, fuzzy
 
+RANDOM_TESTS = 15
+
 class RandomTest(TestCase):
 	"""
 	Test the random functions
@@ -12,14 +14,37 @@ class RandomTest(TestCase):
 		Check status is 'ok' by default
 		"""
 		arr = ["a", "b", "c"]
-		for i in range(10):
+		for i in range(RANDOM_TESTS):
 			self.assertIn(random_in(arr), arr)
 
 	def test_random_value(self):
 		min = 0
 		max = 5
 
-		for i in range(10):
+		for i in range(RANDOM_TESTS):
 			v = random_value(min, max)
 			self.assertTrue(min <= v)
 			self.assertTrue(v <= max)
+
+	def test_random_gauss(self):
+		min = 0
+		max = 5
+
+		for i in range(RANDOM_TESTS):
+			v = random_value(min, max)
+			self.assertTrue(min <= v)
+			self.assertTrue(v <= max)
+
+	def test_random_die(self):
+		max = 6
+		for i in range(RANDOM_TESTS):
+			v = random_die(max)
+			self.assertTrue(v >= 1)
+			self.assertTrue(v <= max)
+
+	def test_random_fuzzy(self):
+		max = 6
+		for i in range(RANDOM_TESTS):
+			v = fuzzy(max)
+			self.assertTrue(v >= -5)
+			self.assertTrue(v <= 5)
