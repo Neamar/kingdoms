e56967236a7d26897cbf38eaa4f6875f87d958ed
Neamar <neamar@neamar.fr>
2013-11-09 20:04:34 +0100

    Remove PEA from admin

--- event/admin.py
+++ event/admin.py
@@ -46,16 +46,3 @@ class PendingEventAdmin(admin.ModelAdmin):
 	list_display = ('event', 'kingdom', 'started', 'text')
 	search_fields = ('event__name', 'text')
 admin.site.register(PendingEvent, PendingEventAdmin)
-
-
-class PendingEventActionAdmin (admin.ModelAdmin):
-	list_display = ('pending_event', 'text', 'folk')
-	search_fields = ('event__name', 'text')
-	actions = ['fire']
-
-	def fire(self, request, queryset):
-		for pendingevent in queryset:
-			pendingevent.fire()
-		self.message_user(request, "Les évènements ont été résolus")
-	fire.short_description = "Résoudre les évènements sélectionnés"
-admin.site.register(PendingEventAction, PendingEventActionAdmin)
