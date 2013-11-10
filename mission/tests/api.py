d61f447ad4abef2828aca25a81d113fb27602a93
Neamar <neamar@neamar.fr>
2013-11-10 23:05:26 +0100

    More tests for mission

--- mission/tests/api.py
+++ mission/tests/api.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 
 from kingdom.models import Kingdom, Folk
 from title.models import Title, AvailableTitle
-from mission.models import Mission, MissionGrid, PendingMission, PendingMissionAffectation
+from mission.models import Mission, MissionGrid, PendingMission, PendingMissionAffectation, AvailableMission
 
 
 @override_settings(PASSWORD_HASHERS = ('django.contrib.auth.hashers.SHA1PasswordHasher',))
@@ -67,7 +67,7 @@ class ApiTest(TestCase):
 
 	def test_grid_affect(self):
 		"""
-		A folk can't be affected twice to a mission.
+		A folk can be affected
 		"""
 
 		r = self.c.post(reverse('mission.views.pending_mission_grid_affect', args=(self.pm.pk, self.mg.pk)), {'folk': self.f.pk})
@@ -75,7 +75,7 @@ class ApiTest(TestCase):
 
 	def test_grid_defect(self):
 		"""
-		A folk can't be affected twice to a mission.
+		A folk can be defected
 		"""
 		pma = PendingMissionAffectation(
 			pending_mission=self.pm,
@@ -85,4 +85,54 @@ class ApiTest(TestCase):
 		pma.save()
 
 		r = self.c.post(reverse('mission.views.pending_mission_grid_defect', args=(pma.pk,)))
+		self.assertEqual(200, r.status_code)
+
+	def test_target(self):
+		"""
+		A target can be defined
+		"""
+		self.m.has_target = True
+		self.m.save()
+
+		k2 = Kingdom()
+		k2.save()
+
+		r = self.c.post(reverse('mission.views.pending_mission_set_target', args=(self.pm.pk,)), {'target': k2.pk})
+		self.assertEqual(200, r.status_code)
+
+	def test_value(self):
+		"""
+		A value can be defined
+		"""
+		self.m.has_value = True
+		self.m.save()
+
+		r = self.c.post(reverse('mission.views.pending_mission_set_value', args=(self.pm.pk,)), {'value': 100})
+		self.assertEqual(200, r.status_code)
+
+	def test_start(self):
+		"""
+		PendingMission can be started
+		"""
+		r = self.c.post(reverse('mission.views.pending_mission_start', args=(self.pm.pk,)))
+		self.assertEqual(200, r.status_code)
+
+	def test_cancel(self):
+		"""
+		PendingMission can be cancelled
+		"""
+		r = self.c.post(reverse('mission.views.pending_mission_cancel', args=(self.pm.pk,)))
+		self.assertEqual(200, r.status_code)
+
+	def test_availablemission_start(self):
+		"""
+		AvailableMission can be started
+		"""
+		am = AvailableMission(
+			mission=self.m,
+			kingdom=self.k
+		)
+		am.save()
+
+		r = self.c.post(reverse('mission.views.available_mission_start', args=(am.pk,)))
 		self.assertEqual(200, r.status_code)
