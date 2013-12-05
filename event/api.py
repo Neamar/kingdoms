d6099ee104074a6676e1527a1a63c1c807f88be5
Neamar <neamar@neamar.fr>
2013-12-06 00:03:14 +0100

    Display tokens count in API

--- event/api.py
+++ event/api.py
@@ -1,6 +1,5 @@
 from event.serializers import serialize_pending_event
 
-
 def api(request):
 	"""
 	JSON contribution to /api/kingdom from this app.
@@ -10,5 +9,6 @@ def api(request):
 
 	pending_events = request.user.kingdom.pendingevent_set.filter(is_started=True).select_related("event", "event__category").prefetch_related("pendingeventaction_set")
 	resp['pending_events'] = [serialize_pending_event(o) for o in pending_events]
+	resp['pending_events_tokens_count'] = request.user.kingdom.pendingeventtoken_set.count()
 
 	return resp
